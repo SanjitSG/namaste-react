f03d211bf98570e514223f788168675a4e4c9b02
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-03-16 17:16:01 +0530

    laying Foundation

--- 03 - Laying Foundation/code/App.js
+++ 03 - Laying Foundation/code/App.js
@@ -1,35 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const para = React.createElement(
-  "p",
-  {
-    id: "para",
-  },
-  "This is para"
-);
+const Title = function () {
+  return <h1>i'm Title Rendered as Function. </h1>;
+};
 
-const Title = () => <h1>i'm title </h1>;
-
-const HeaderComponent2 = () => (
-  <div>
-    <h1>Header component 2</h1>
-    <h2>This is h2</h2>
-    <p>Im invoked as function</p>
-    {Title()}
-    <hr />
-  </div>
-);
-
-const HeaderComponent3 = () => (
-  <div>
-    <h1>Header component 3</h1>
-    <h2>This is h3</h2>
-    {<Title />}
-    {para}
-  </div>
-);
+const TitleOne = () => {
+  return <h1>i'm Title Rendered as JSX component.</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render([<HeaderComponent2 />, <HeaderComponent3 />]);
+root.render([Title(), <TitleOne />]);
