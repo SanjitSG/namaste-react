8ff5873d2329ce449c5d469fc63bd9fae1634de3
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-03-17 21:12:28 +0530

    assignment

--- 03 - Laying Foundation/code/App.js
+++ 03 - Laying Foundation/code/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = function () {
-  return <h1>i'm Title Rendered as Function. </h1>;
-};
+// react element written using jsx expression
+const title = (
+  <h1 id="title" key="1">
+    Heading
+  </h1>
+);
 
-const TitleOne = () => {
-  return <h1>i'm Title Rendered as JSX component.</h1>;
-};
+//component written using JSX
+const TitleOne = () => (
+  <div>
+    {title}
+    <h1>i'm Title Rendered as JSX component.</h1>
+  </div>
+);
 
+console.log(title);
+console.log(TitleOne);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render([Title(), <TitleOne />]);
+root.render(<TitleOne />);
