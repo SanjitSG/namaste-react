b355bb6a63168f9f36705d212a29ba4959c36449
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-04-04 11:58:54 +0530

    react lifecycle method

--- 08 - Lets get classy/code/src/components/Support.js
+++ 08 - Lets get classy/code/src/components/Support.js
@@ -1,20 +1,33 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const Support = () => {
-  return (
-    <div>
-      <h1>Support Component</h1>
-      <User
-        name={"Sanjit Functional"}
-        location={"Goa"}
-      />
-      <UserClass
-        name={"Sanjit Class"}
-        location={"BLR"}
-      />
-    </div>
-  );
-};
+class Support extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>Support Component</h1>
+        <UserClass
+          name={"First Class Component"}
+          location={"Goa"}
+        />
+        <UserClass
+          name={"Second Class Component"}
+          location={"BLR"}
+        />
+      </div>
+    );
+  }
+}
 
 export default Support;
--- 08 - Lets get classy/code/src/components/UserClass.js
+++ 08 - Lets get classy/code/src/components/UserClass.js
@@ -8,14 +8,29 @@ class UserClass extends React.Component {
       count: 0,
       count2: 1,
     };
+    console.log(this.props.name + "Child Constructor");
+  }
+
+  componentDidMount() {
+    console.log(this.props.name + "Child componentDidMount");
   }
   render() {
+    console.log(this.props.name + "Child Render");
     const { name, location } = this.props;
     const { count2 } = this.state;
     return (
       <div className="user-card">
         <h1>Count: {this.state.count}</h1>
-        <h1>Count: {count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Increase count
+        </button>
+
         <h3>Name: {name}</h3>
         <h4>Location: {location}</h4>
         <h4>Social: @SanjitGoa</h4>
