0fa777e10d029251898784fdb077cc1313716be8
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-04-28 21:51:21 +0530

    tmdb api call

--- 14 - NetflixGPT/netflix-gpt/src/utils/constants.js
+++ 14 - NetflixGPT/netflix-gpt/src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTUyNDFiMzczMmY1YTRjZjVkNWY1Y2NiYWUwMzVmMyIsInN1YiI6IjY2MmU3M2VhMjRmMmNlMDEyMzJhY2FlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P8GLL-9bWQpNhLeBBOR6xUae8TrGgBqoofzhRACrE_k",
+  },
+};
