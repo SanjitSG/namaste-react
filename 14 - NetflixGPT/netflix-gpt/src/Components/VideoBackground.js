0b121778514197f5d48dd0cad5d958e4648193d2
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-04-29 18:39:40 +0530

    main container

--- 14 - NetflixGPT/netflix-gpt/src/Components/VideoBackground.js
+++ 14 - NetflixGPT/netflix-gpt/src/Components/VideoBackground.js
@@ -8,10 +8,15 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
-        src={"https://www.youtube.com/embed/" + trailerKey}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube-nocookie.com/embed/" + trailerKey + "?controls=0&autoplay=1&mute=1"
+        }
         title="YouTube video player"
+        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
       ></iframe>
     </div>
   );
--- 14 - NetflixGPT/netflix-gpt/src/Components/VideoTitle.js
+++ 14 - NetflixGPT/netflix-gpt/src/Components/VideoTitle.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="absolute w-screen aspect-video pt-[18%] px-24 text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold w-1/2">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <p className="py-6 text-lg w-1/3">{overview}</p>
       <div>
-        <button className="bg-slate-50 text-3xl font-semibold px-8 py-4 rounded-lg mx-2">
+        <button className="bg-slate-50 text-3xl font-semibold px-6 py-2 rounded-lg text-black hover:bg-opacity-90">
           &#9654; Play
         </button>
-        <button className="bg-slate-200 text-white font-semibold opacity-70 text-3xl px-8 py-4 rounded-lg mx-2">
-          &#x21; More Info
+        <button className=" bg-gray-500 bg-opacity-50 font-semibold opacity-70 text-3xl px-6 py-2 rounded-lg mx-2 hover:bg-opacity-25">
+          More Info
         </button>
       </div>
     </div>
