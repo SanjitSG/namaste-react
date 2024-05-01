be0c38fdb723811274f57945da0c3f8cfaa3c360
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-05-01 20:18:41 +0530

    gpt search

--- 14 - NetflixGPT/netflix-gpt/src/utils/constants.js
+++ 14 - NetflixGPT/netflix-gpt/src/utils/constants.js
@@ -23,5 +23,3 @@ export const supported_languages = [
   { identifier: "hindi", language: "Hindi" },
   { identifier: "konkani", language: "Konkani" },
 ];
-
-export const OPENAI_KEY = "sk-proj-dY64SPMm8bBwiCedXJSCT3BlbkFJwujN4La8TRYPzNj1wWnY";
